2cd9b37bf94ea62eeec10f9e3efd46fc4f2ff5a9
Tyler Holcombe <tyler.holcombe111@gmail.com>
2020-10-11 18:21:22 -0700

    Generate component for main page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { JoinComponent } from './join/join.component';
+import { MainComponent } from './main/main.component';
 import { SuccessComponent } from './join/success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JoinComponent,
+    MainComponent,
     SuccessComponent
   ],
   imports: [
