f9fa426f378ae6d8763ee16d43264f805be4bacb
Tyler Holcombe <tyler.holcombe111@gmail.com>
2020-10-11 18:55:25 -0700

    Add table to main page (with mock data).

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { JoinComponent } from './join/join.component';
+import { MainComponent } from './main/main.component';
 import { SuccessComponent } from './join/success/success.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'join', pathMatch: 'full'},
+  {path: '', redirectTo: 'main', pathMatch: 'full'},
   {path: 'join', component: JoinComponent},
+  {path: 'main', component: MainComponent},
   {path: 'success', component: SuccessComponent}
 ];
 
