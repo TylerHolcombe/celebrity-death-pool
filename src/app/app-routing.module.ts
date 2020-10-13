5ea8dcc4148d787c3daefb6ab9d96b8fdb74adcd
Tyler Holcombe <tyler.holcombe111@gmail.com>
2020-10-12 21:29:08 -0700

    Generate admin navbar component and add child routes for admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AdminComponent } from './admin/admin.component';
 import { JoinComponent } from './join/join.component';
 import { MainComponent } from './main/main.component';
 import { SuccessComponent } from './join/success/success.component';
+import { TableComponent } from './main/table.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: 'admin', component: AdminComponent, children: [
+    {path: 'players', component: TableComponent},
+  ]},
   {path: 'join', component: JoinComponent},
   {path: 'main', component: MainComponent},
-  {path: 'success', component: SuccessComponent}
+  {path: 'success', component: SuccessComponent},
 ];
 
 @NgModule({
