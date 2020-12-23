4abc96319406757804d9a857b20d886728cff8f5
tholcombe <tyler.holcombe111@gmail.com>
2020-12-23 17:45:42 -0500

    Allow update of accepted entries (to be pending)

--- src/app/admin/players/players.component.ts
+++ src/app/admin/players/players.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 
 import { Entry } from '../../player';
+import { TableComponent } from '../../main/table/table.component';
 import { PlayerService } from '../../player.service';
 
 @Component({
@@ -8,16 +9,33 @@ import { PlayerService } from '../../player.service';
   templateUrl: './players.component.html',
   styleUrls: ['./players.component.css']
 })
-export class PlayersComponent implements OnInit {
+export class PlayersComponent implements AfterViewInit {
+  @ViewChild("acceptedTable")
+  private tableComponent: TableComponent;
+
   entries: Entry[];
 
   constructor(private playerService: PlayerService) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.getEntries();
   }
 
   getEntries(): void {
     this.playerService.getApprovedEntries().subscribe(entries => this.entries = entries);
+  }
+
+  saveEntries(): void {
+    let saveEntries: Entry[] = [];
+    for (let index in this.tableComponent.updatedEntries) {
+      if (this.tableComponent.updatedEntries[index]) {
+        saveEntries.push(this.tableComponent.entries[index]);
+      }
+    }
+
+    this.playerService.saveEntries(saveEntries).subscribe(status => {
+      this.tableComponent.updatedEntries.fill(false);
+      this.getEntries();
+    });
   }
 }
